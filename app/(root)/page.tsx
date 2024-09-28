4de959dae11165065799311a16ec8ab53c27040e
Muhammed Sahad P <zanusahad@gmail.com>
2024-09-28 17:59:05 +0530

    implemen user button

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,5 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
-  return (
-    <main>
-       <h1>Threads</h1>
-    </main>       
-  );
+  return <UserButton afterSignOutUrl="/" />;
 }
